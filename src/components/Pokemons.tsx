c0dfed12274ec9be2234dfed8e0b6db75cc38805
darkbooker <raelvirod@hotmail.com>
2023-11-04 13:37:18 -0400

    Sprites

--- src/components/Pokemons.tsx
+++ src/components/Pokemons.tsx
@@ -38,8 +38,8 @@ const Items = ({ pokemons }: Props) => {
                 <Image
                   alt={`image of ${pokemon.name}`}
                   src={`https://bzgzlhdowumbsdyxqlso.supabase.co/storage/v1/object/public/pokemonimages/${pokemon.image}`}
-                  width={64}
-                  height={64}
+                  width={52}
+                  height={52}
                   className='rounded'
                 />
               </Box>
