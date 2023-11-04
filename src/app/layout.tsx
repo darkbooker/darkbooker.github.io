425c230c59ca6ea495bf063404e18bd4a157514b
darkbooker <raelvirod@hotmail.com>
2023-11-04 17:44:45 -0400

    theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,19 @@ import "./globals.css";
 import { Box, Container } from "@radix-ui/themes";
 import SearchComponent from "@/components/SearchComponent";
 import "@radix-ui/themes/styles.css";
-import { Theme, Heading } from "@radix-ui/themes";
+import { Theme, Heading, ThemeProvider } from "@radix-ui/themes";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+
+// Define your custom theme with the accent color
+const customTheme = {
+  ...dark,
+  colors: {
+    ...dark.colors,
+    accent: "#b64444", // Set the accent color to #b64444
+  },
+};
 
 export const metadata: Metadata = {
   title: "Pokemon Searcher",
@@ -16,13 +25,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
-        <Theme appearance='dark'>
+        <ThemeProvider theme={customTheme}> {/* Wrap your app with ThemeProvider */}
           <Box>
-            <Container className='px-2'>
-              <Heading className='py-2'>
-                <Link href='/' className='text-white no-underline'>
+            <Container className="px-2">
+              <Heading className="py-2">
+                <Link href="/" className="text-white no-underline">
                   Radical Red Movesets
                 </Link>
               </Heading>
@@ -30,7 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               {children}
             </Container>
           </Box>
-        </Theme>
+        </ThemeProvider>
       </body>
     </html>
   );
