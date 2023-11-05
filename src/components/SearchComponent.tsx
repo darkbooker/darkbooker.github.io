c8ef66727900fcc3de499ec822742f8361603182
darkbooker <raelvirod@hotmail.com>
2023-11-04 20:07:27 -0400

    keyboard navigation

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -20,15 +20,40 @@ export default function SearchComponent() {
   const [search, setSearch] = useState("");
   const defferedSearch = useDeferredValue(search);
   const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+  const [highlightedOption, setHighlightedOption] = useState(-1); // Added state for highlighting
 
   const handleChange = async (value: string) => {
     setSearch(value);
+    setHighlightedOption(-1); // Reset highlighted option when search input changes
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (search) {
       router.push(`/search?q=${search}`);
+    }
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (pokemons && pokemons.length > 0) {
+      switch (e.key) {
+        case 'ArrowDown':
+          e.preventDefault(); // Prevent scrolling the page
+          setHighlightedOption((prev) => (prev + 1) % pokemons.length);
+          break;
+        case 'ArrowUp':
+          e.preventDefault(); // Prevent scrolling the page
+          setHighlightedOption((prev) => (prev - 1 + pokemons.length) % pokemons.length);
+          break;
+        case 'Enter':
+          if (highlightedOption !== -1) {
+            router.push(`/${pokemons[highlightedOption].name.toLowerCase()}`);
+            setSearch('');
+          }
+          break;
+        default:
+          break;
+      }
     }
   };
 
@@ -49,6 +74,7 @@ export default function SearchComponent() {
           </TextField.Slot>
           <TextField.Input
             onChange={(e) => handleChange(e.target.value)}
+            onKeyDown={handleKeyDown}
             placeholder='Search...'
             value={search}
           />
@@ -60,21 +86,13 @@ export default function SearchComponent() {
           <Box className='rounded-b-md rounded-bl-md relative w-full shadow-lg z-10'>
             <Box className='absolute bg-[#17191a] rounded-b-md rounded-bl-md py-1 w-full '>
               {pokemons
-                ?.filter((item) => {
-                  const searchValue = search.toLowerCase();
-                  return (
-                    searchValue[0] == item.name.toLowerCase()[0] &&
-                    searchValue &&
-                    item.name.toLowerCase().includes(searchValue)
-                  );
-                })
-                .map((pokemon: Pokemon) => (
+                ?.map((pokemon: Pokemon, index) => (
                   <Box
-                    className='py-1 px-8 hover:bg-slate-700 active:bg-slate-600 opacity-80 rounded-b-md rounded-bl-md '
+                    className={`py-1 px-8 ${index === highlightedOption ? 'hover:bg-slate-700 active:bg-slate-600 opacity-80 rounded-b-md rounded-bl-md' : ''}`}
                     key={pokemon.id}
                     onClick={() => {
                       router.push(`/${pokemon.name.toLowerCase()}`);
-                      setSearch("");
+                      setSearch('');
                     }}
                   >
                     {pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}
