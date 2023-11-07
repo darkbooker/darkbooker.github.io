10de922d9c42b609c9e8f9bc97467cfa3c735bc6
darkbooker <raelvirod@hotmail.com>
2023-11-07 07:11:05 -0500

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
 import { Box } from "@radix-ui/themes";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeSanitize from "rehype-sanitize";
-import rehypeStringify from "rehype-stringify";
 
-const Page = async () => {
-  const yourText = `
-    <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" /> <img src="image2.jpg" alt="Image 2" /> <img src="image3.jpg" alt="Image 3" /> <img src="image4.jpg" alt="Image 4" />
-    <img src="image5.jpg" alt="Image 5" /> <img src="image6.jpg" alt="Image 6" /> <img src="image7.jpg" alt="Image 7" /> <img src="image8.jpg" alt="Image 8" />
-    
-    Welcome to the Radical Red Movesets Database!
-    
-    Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.
-  `;
-
-  const file = await unified()
-    .use(remarkParse) // Convert into markdown AST
-    .use(remarkRehype) // Transform to HTML AST
-    .use(rehypeSanitize) // Sanitize HTML input
-    .use(rehypeStringify) // Convert AST into serialized HTML
-    .process(yourText);
-
-  const html = String(file);
-
+const Page = () => {
   return (
-    <Box className='py-3'>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
+    <Box className="py-3">
+      <div>
+        <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
+        <img src="image2.jpg" alt="Image 2" />
+        <img src="image3.jpg" alt="Image 3" />
+        <img src="image4.jpg" alt="Image 4" />
+        <img src="image5.jpg" alt="Image 5" />
+        <img src="image6.jpg" alt="Image 6" />
+        <img src="image7.jpg" alt="Image 7" />
+        <img src="image8.jpg" alt="Image 8" />
+      </div>
+      <div>Welcome to the Radical Red Movesets Database!</div>
+      <div>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
     </Box>
   );
 };
