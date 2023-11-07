a5b3e5ccbbd8244e5264736ff9e0429fb974e80d
darkbooker <raelvirod@hotmail.com>
2023-11-07 07:44:26 -0500

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { Box } from "@radix-ui/themes";
-
 const Page = () => {
   return (
     <Box className="py-3" style={{ textAlign: "center" }}>
@@ -15,8 +13,8 @@ const Page = () => {
           <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
         </div>
       </div>
-      <div style="font-weight: bold; font-size: 18px;">Welcome to the Radical Red Movesets Database!</div>
-<div style="font-style: italic; font-size: 10px;">Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
+      <div>Welcome to the Radical Red Movesets Database!</div>
+      <div>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
     </Box>
   );
 };
