2e6c1d032df9772d58086bdc9a0e25e8e563ca89
darkbooker <raelvirod@hotmail.com>
2023-11-07 07:28:02 -0500

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,23 @@
 import { Box } from "@radix-ui/themes";
-import { FlexDirection } from "@radix-ui/css";
-
-const containerStyle = {
-  display: "flex",
-  flexDirection: FlexDirection.Column,
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-};
 
 const Page = () => {
   return (
-    <div style={containerStyle}>
-      <Box className="py-3">
-        <div>
-        <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
-        <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
-        <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
-        <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
-        <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
-        <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
-        <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
-        <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
+    <Box className="py-3" style={{ textAlign: "center" }}>
+      <div>
+        <div style={{ margin: "0 auto", textAlign: "center" }}>
+          <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
+          <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
+          <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
+          <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
+          <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
+          <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
+          <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
+          <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
         </div>
-        <div>Welcome to the Radical Red Movesets Database!</div>
-        <div>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
-      </Box>
-    </div>
+      </div>
+      <div>Welcome to the Radical Red Movesets Database!</div>
+      <div>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
+    </Box>
   );
 };
 
