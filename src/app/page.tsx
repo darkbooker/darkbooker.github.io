3d10bc8d39405115e2686f56e644a11b4fb8af20
darkbooker <raelvirod@hotmail.com>
2023-11-07 06:56:13 -0500

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,30 @@
 import { Box } from "@radix-ui/themes";
 import { unified } from "unified";
-import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
-import path from "path";
-import fs from "fs";
+import React from "react";
 
 const Page = async () => {
-  const fullPath = path.resolve(process.cwd(), "README.md");
-  const fileContents = fs.readFileSync(fullPath, "utf8");
+  // Define your custom content with 8 images in the first line and text on the second and third lines
+  const customContent = `
+    <img src="image1.jpg" alt="Image 1" />
+    <img src="image2.jpg" alt="Image 2" />
+    <img src="image3.jpg" alt="Image 3" />
+    <img src="image4.jpg" alt="Image 4" />
+    <img src="image5.jpg" alt="Image 5" />
+    <img src="image6.jpg" alt="Image 6" />
+    <img src="image7.jpg" alt="Image 7" />
+    <img src="image8.jpg" alt="Image 8" />
+    <p>Welcome to the Radical Red Movesets Database!</p>
+    <p>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</p>
+  `;
+
   const file = await unified()
-    .use(remarkParse) // Convert into markdown AST
     .use(remarkRehype) // Transform to HTML AST
     .use(rehypeSanitize) // Sanitize HTML input
     .use(rehypeStringify) // Convert AST into serialized HTML
-    .process(fileContents);
+    .process(customContent);
   const html = String(file);
 
   // Center the content both horizontally and vertically with CSS Flexbox
