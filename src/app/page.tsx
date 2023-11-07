5cab6fd8e5dd95b66469cca470d0ba78d9fc38b5
darkbooker <raelvirod@hotmail.com>
2023-11-07 08:18:48 -0500

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,41 @@
-import React from 'react';
-import { Box, Text, Grid, Heading } from '@radix-ui/themes';
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Radical Red Movesets Database</title>
+    <style>
+        body {
+            text-align: center; /* Center horizontally */
+            margin: 20px auto; /* Margin at the top to center vertically */
+            max-width: 800px; /* Optional: Set a maximum width for content */
+        }
 
-const Homepage = () => {
-  return (
-    <div className="homepage">
-      {/* Grid for Images */}
-      <Grid
-        style={{
-          gap: 1,
-          gridTemplateColumns: 'repeat(4, 1fr)',
-          alignItems: 'center',
-          justifyItems: 'center',
-        }}
-      >
-        <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
-        <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
-        <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
-        <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
-        <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
-        <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
-        <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
-        <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
-      </Grid>
+        #header {
+            text-align: center; /* Center the text within the header */
+        }
 
-      <Box style={{ padding: 20, backgroundColor: 'black', textAlign: 'center' }}>
-        <Heading size="7">Welcome to the Radical Red Movesets Database!</Heading>
-      </Box>
-
-      <Box style={{ padding: 20, backgroundColor: 'black', textAlign: 'center' }}>
-        <Text>
-          Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.
-        </Text>
-      </Box>
+        #image-container {
+            display: flex;
+            justify-content: center; /* Center the images horizontally within the container */
+        }
+    </style>
+</head>
+<body>
+    <div id="header">
+        <!-- First Line: 8 Images -->
+        <div id="image-container">
+            <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
+            <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
+            <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
+            <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
+            <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
+            <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
+            <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
+            <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
+        </div>
+        <!-- Second Line: Welcome Message -->
+        <h1>Welcome to the Radical Red Movesets Database!</h1>
+        <!-- Third Line: Reporting Instructions -->
+        <p>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</p>
     </div>
-  );
-};
-
-export default Homepage;
+</body>
+</html>
