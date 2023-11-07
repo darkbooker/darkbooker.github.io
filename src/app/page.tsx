dd8ec87258cdf3e11511f9b80faeb5a7f2a3b5d6
darkbooker <raelvirod@hotmail.com>
2023-11-07 08:03:01 -0500

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,39 @@
-const Page = () => {
+import React from 'react';
+import { Box, Text, Grid, Heading } from '@radix-ui/themes';
+
+const Homepage = () => {
   return (
-    <Box className="py-3" style={{ textAlign: "center" }}>
-      <div>
-        <div style={{ margin: "0 auto", textAlign: "center" }}>
-          <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
-          <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
-          <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
-          <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
-          <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
-          <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
-          <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
-          <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
-        </div>
-      </div>
-      <div>Welcome to the Radical Red Movesets Database!</div>
-      <div>Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.</div>
-    </Box>
+    <div className="homepage">
+      {/* Grid for Images */}
+      <Grid
+        css={{
+          gap: 20,
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          alignItems: 'center',
+          justifyItems: 'center',
+        }}
+      >
+        <img src="https://i.ibb.co/G5VcKdJ/download.png" alt="houndoom" />
+        <img src="https://i.ibb.co/8dKTmfq/download-1.png" alt="sceptile" />
+        <img src="https://i.ibb.co/XJtJ8xs/download.png" alt="ironbundle" />
+        <img src="https://i.ibb.co/dtgsyvv/dialga.png" alt="dialga" />
+        <img src="https://i.ibb.co/0VRXbqw/drednaw.png" alt="drednaw" />
+        <img src="https://i.ibb.co/hHyQd5B/noivern.png" alt="noivern" />
+        <img src="https://i.ibb.co/Jt3TJ05/pikachu.png" alt="pikachu" />
+        <img src="https://i.ibb.co/xMRT3T7/toxicroak.png" alt="toxicroak" />
+      </Grid>
+
+      <Box css={{ padding: 20, backgroundColor: 'black' }}>
+        <Heading size={15}>Welcome to the Radical Red Movesets Database!</Heading>
+      </Box>
+
+      <Box css={{ padding: 20, backgroundColor: 'black' }}>
+        <Text>
+          Report Mistakes in the Documentation-Reports channel in the Radical Red Discord Server.
+        </Text>
+      </Box>
+    </div>
   );
 };
 
-export default Page;
+export default Homepage;
